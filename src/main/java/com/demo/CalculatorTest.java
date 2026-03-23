fd5cd2c305a0dcd15075080489598177aa7807d4
Vieetj-Anh <hovietanh2812@gmail.com>
2026-03-23 09:07:17 +0700

    test pipeline

--- src/main/java/com/demo/CalculatorTest.java
+++ src/main/java/com/demo/CalculatorTest.java
@@ -1,14 +1,14 @@
 package com.demo;
 
-
+import org.junit.Test;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class CalculatorTest {
 
-    @org.testng.annotations.Test
-    void testAdd() {
+    @Test
+    public void testAdd() {
         Calculator cal = new Calculator();
         assertEquals(5, cal.add(2,3));
-        assertEquals(999, cal.add(2,3));
+        assertEquals(999, cal.add(2,3)); // FAIL
     }
 }
